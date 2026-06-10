b39988784264a5b333a502c958118f77772b8bf9
wrobeloliver2-crypto <wrobeloliver2@gmail.com>
2026-06-10 16:44:08 +0200

    Osteo-Check Terminart-Erweiterung + Enter-Login

--- netlify/functions/list-appointments.js
+++ netlify/functions/list-appointments.js
@@ -9,7 +9,7 @@ exports.handler = async (event)=>{
       id:a.id, firstName:a.firstName, lastName:a.lastName, email:a.email,
       cc:a.cc, phone:a.phone, date:a.date, time:a.time, practitioner:a.practitioner,
       confirmSent:a.confirmSent, reminder3dSent:a.reminder3dSent, reminder24hSent:a.reminder24hSent,
-      status:a.status||'active', cancelledAt:a.cancelledAt||''
+      status:a.status||'active', cancelledAt:a.cancelledAt||'', type:a.type||'osteo'
     }));
     return resp(200,{appointments});
   }catch(e){
